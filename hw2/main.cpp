8f24bd0c0e60f947e33b020a9c46ee7092387347
Keegan Smith <keeganasmith2003@gmail.com>
2026-01-28 23:03:23 -0600

    hw 2 like halfway done i guess

---
+++ hw2/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    string filepath = "../data/ufos.csv";
+    ifstream my_file(filepath);
+
+    string headers_raw;
+    getline(my_file, headers_raw);
+    
+}
